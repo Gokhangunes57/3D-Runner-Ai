38eff0527914ab41aefd17c13b56c1f030dfe304
Gökhan güneş <132ggokhann@gmail.com>
2023-03-18 21:45:05 +0300

    agent has been moved

--- Runner AI/Assets/Scripts/ClickToMove.cs
+++ Runner AI/Assets/Scripts/ClickToMove.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class ClickToMove : MonoBehaviour
 {
+   NavMeshAgent agent;
+   RaycastHit hit = new RaycastHit();
+   
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin,ray.direction, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+       
     }
 }
