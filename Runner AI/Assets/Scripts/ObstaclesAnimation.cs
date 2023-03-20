d329a986297820b0915348cccc549ededc5fec63
Gökhan güneş <132ggokhann@gmail.com>
2023-03-20 20:05:17 +0300

    Obsanimation script added

---
+++ Runner AI/Assets/Scripts/ObstaclesAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesAnimation : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float strength = 2.5f;
+    private float RandomOffset;
+    
+    
+        
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
